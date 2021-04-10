cc3e3a9847b9a9067e2b1821f35870ca04e1dde0
Your Name <you@example.com>
2021-04-10 15:09:08 -0400

    change route of deals

--- market-service/routes/deals.js
+++ market-service/routes/deals.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/greet', function(req, res, next) {
-  res.send('works');
+router.get('/', function(req, res, next) {
+  res.send('Deals route');
 });
 
 module.exports = router;

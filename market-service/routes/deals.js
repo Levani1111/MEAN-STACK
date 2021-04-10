020057ec84e2805dc1875f6b217a672be95fa06d
Your Name <you@example.com>
2021-04-10 16:39:52 -0400

    add images on to cards

--- market-service/routes/deals.js
+++ market-service/routes/deals.js
@@ -5,16 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   let jsonResponse = {
    'handsetCards': [
-    { title: 'Card 1', cols: 2, rows: 1 },
-    { title: 'Card 2', cols: 2, rows: 1 },
-    { title: 'Card 3', cols: 2, rows: 1 },
-    { title: 'Card 4', cols: 2, rows: 1 }
+    { imageName: 'imge1.jpg', title: '10% off on personal cares', cols: 2, rows: 1 },
+    { imageName: 'imge2.jpg', title: 'Extende warranty for headphones', cols: 2, rows: 1 },
+    { imageName: 'imge3.jpg', title: '5% discount for POEME', cols: 2, rows: 1 },
+    { imageName: 'imge4.jpg', title: 'Bets 8% of watch', cols: 2, rows: 1 }
   ],
    'webCards': [
-    { title: 'Card 1', cols: 2, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 3', cols: 1, rows: 2 },
-    { title: 'Card 4', cols: 1, rows: 1 }
+    { imageName: 'imge1.jpg', title: '10% off on personal cares', cols: 2, rows: 1 },
+    { imageName: 'imge2.jpg', title: 'Extende warranty for headphones', cols: 1, rows: 1 },
+    { imageName: 'imge3.jpg', title: '5% discount for POEME', cols: 1, rows: 2 },
+    { imageName: 'imge4.jpg', title: 'Bets 8% of watch', cols: 1, rows: 1 }
   ],
   };
   res.json(jsonResponse);
